5fbf878f4dfaac4a8afa5dfcd0bf9cacf99e0d98
Nikhil Choudhary <nc1336282@gmail.com>
2020-09-25 19:25:21 +0530

    Updates

---
+++ precache-manifest.87e98056f01eec8401caf1a851cfa184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4582412f85e38308c96b2c7198bfbef6",
+    "url": "/react-dnd/index.html"
+  },
+  {
+    "revision": "d0bd2cc698e2f3c4c06a",
+    "url": "/react-dnd/static/css/main.79d99b21.chunk.css"
+  },
+  {
+    "revision": "0a162479f8e5a4ebce05",
+    "url": "/react-dnd/static/js/2.64402dc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dnd/static/js/2.64402dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0bd2cc698e2f3c4c06a",
+    "url": "/react-dnd/static/js/main.a106c6bb.chunk.js"
+  },
+  {
+    "revision": "095c49c345ae6e238cc2",
+    "url": "/react-dnd/static/js/runtime-main.2261a173.js"
+  }
+]);
